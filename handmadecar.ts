7c6ad4835d88bc0afeca4951172390cc225db30b
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2023-09-05 12:41:33 +0900

    更新 handmadecar.ts

--- handmadecar.ts
+++ handmadecar.ts
@@ -172,7 +172,7 @@ namespace eureka_blocks_car {
 
 
     //% color="#3943c6" weight=63blockId=servos_lotation
-    //% block="回転 |%lot_houkou| " group="4 高度なブロック"
+    //% block="回転 |%lot_houkou| " group="2　基本の動き"
     export function car_lotation(lot_houkou: lotation): void {
         switch (lot_houkou) {
             case lotation.右:
